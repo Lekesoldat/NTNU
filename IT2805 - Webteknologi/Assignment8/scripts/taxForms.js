25417d66058158b6f7e8ce2af44714f47a543734
Magnus L. Holtet <magnulho@gmail.com>
2018-10-21 22:20:56 +0200

    Initial commit

---
+++ IT2805 - Webteknologi/Assignment8/scripts/taxForms.js
@@ -0,0 +1,17 @@
+const taxForms = [
+  {
+    realName: "Bruce Wayne",
+    income: 750000,
+    wealth: 300000
+  },
+  {
+    realName: "John Blake",
+    income: 440000,
+    wealth: 832000
+  },
+  {
+    realName: "Selina Kyle",
+    income: 640000,
+    wealth: 432000
+  }
+];
