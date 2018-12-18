171601c65044ede23e9d3383a0b7a8f36f12dded
Magnus L. Holtet <42798691+Lekesoldat@users.noreply.github.com>
2018-12-18 12:50:23 +0100

    Update task3.js

--- IT2805 - Webteknologi/Exams/2017/code/task3.js
+++ IT2805 - Webteknologi/Exams/2017/code/task3.js
@@ -1,9 +1,9 @@
-// Note that month and day are 0-indexed
+// Note that month is 0-indexed
 const today = new Date();
-const christmas = new Date(2018, 11, 25);
+const christmas = new Date(2018, 11, 24);
 
 console.log('Today: ', today);
 console.log('Christmas: ', christmas);
 
-const timeUntilChristmas = Math.floor((christmas - today) / (1000 * 60 * 60 * 24));
+const timeUntilChristmas = Math.ceil((christmas - today) / (1000 * 60 * 60 * 24));
 console.log('Days until christmas: ', timeUntilChristmas);
