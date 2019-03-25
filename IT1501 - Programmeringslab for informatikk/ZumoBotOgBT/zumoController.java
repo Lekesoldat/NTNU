9cc8c0412b0c9d95f485c14568efaef9f8fe2057
Lekesoldat <magnulho@gmail.com>
2019-03-25 20:28:07 +0100

    Update.

--- IT1501 - Programmeringslab for informatikk/ZumoBotOgBT/zumoController.java
+++ IT1501 - Programmeringslab for informatikk/ZumoBotOgBT/zumoController.java
@@ -1,4 +1,3 @@
-package plab.gayshit;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
@@ -10,46 +9,46 @@ import javafx.scene.control.TextField;
 import javafx.scene.paint.Color;
 import plab.serial.PLabSerial;
 
-public class zumoController{
+public class zumoController {
 	private PLabSerial serialConnection = new PLabSerial();
 	@FXML
-    private TextField serialPortNameField;
-	
+	private TextField serialPortNameField;
+
 	@FXML
 	private Button button;
-	
+
 	@FXML
 	void initialize() {
 		String portName;
-		if(serialConnection.openPLabPort()) {	
-		    portName = serialConnection.getOpenPortName();}
-		else {
+		if (serialConnection.openPLabPort()) {
+			portName = serialConnection.getOpenPortName();
+		} else {
 			portName = "No PLab BT device found.";
-		};
+		}
+		;
 		serialPortNameField.setText(portName);
-		serialConnection.addListener(this, "messageReceived");	
+		serialConnection.addListener(this, "messageReceived");
 	}
-	
+
 	public void buttonPressed() {
 		if (button.getText().equals("Lys på")) {
 			button.setText("Lys av");
 			button.setTextFill(Color.RED);
 			turnOff();
-			
+
 		} else {
 			button.setText("Lys på");
 			button.setTextFill(Color.GREEN);
 			turnOn();
 		}
-	
+
 	}
-	
+
 	public void turnOff() {
-	    serialConnection.sendMessage("#buttonPressedOff",0);
+		serialConnection.sendMessage("#buttonPressedOff", 0);
 	}
-	 
-	
+
 	public void turnOn() {
-	    serialConnection.sendMessage("#buttonPressedOn",0);
+		serialConnection.sendMessage("#buttonPressedOn", 0);
 	}
 }
--- IT1501 - Programmeringslab for informatikk/ZumoBotOgBT/zumoMain.java
+++ IT1501 - Programmeringslab for informatikk/ZumoBotOgBT/zumoMain.java
@@ -1,5 +1,3 @@
-package plab.gayshit;
-
 import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -7,7 +5,6 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
- 
 public class zumoMain extends Application {
     @Override
     public void start(Stage primaryStage) throws IOException {
@@ -16,7 +13,7 @@ public class zumoMain extends Application {
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
-       	
+
     public static void main(String[] args) {
         launch(args);
     }
