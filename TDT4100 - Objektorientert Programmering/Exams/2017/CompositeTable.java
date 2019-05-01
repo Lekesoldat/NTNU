5b5b6d72dda6e54fa2be0ebe480c1f2674e17e2a
Magnus L. Holtet <magnulho@gmail.com>
2019-05-01 20:41:22 +0200

    Update.

---
+++ TDT4100 - Objektorientert Programmering/Exams/2017/CompositeTable.java
@@ -0,0 +1,9 @@
+/**
+ * A table that consists of two other tables.
+ */
+public class CompositeTable extends Table {
+       
+  public CompositeTable(Table table1, Table table2, int lostCapacity) {
+    
+  }
+}
