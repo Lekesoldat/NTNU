805ce482eb34f7ccb3782bdafcc0e5e10aed94ed
Magnus L. Holtet <magnulho@gmail.com>
2019-05-19 13:26:10 +0200

    Add stream.

--- TDT4100 - Objektorientert Programmering/Exams/2016/Family.java
+++ TDT4100 - Objektorientert Programmering/Exams/2016/Family.java
@@ -30,40 +30,32 @@ public class Family {
   public Person findMember(String name) {
     return family.stream().filter(p -> p.getName().equals(name)).findFirst().orElse(null);
   }
-  //
 
   /**
-   * Writes the contents of this Family to the OutputStream, so it can be
-   * reconstructed using load.
-   * 
-   * @param out
-   */
+  * Writes the contents of this Family to the OutputStream,
+  * so it can be reconstructed using load.
+  * @param out
+  */
   public void save(OutputStream out) throws IOException {
     try (PrintWriter pw = new PrintWriter(out)) {
-      /*
-      # Members
-      male Jonas
-      female Stella
-      female Karianne
-      # Relations
-      Jonas Stella Johanna Roger
-      */
       pw.println("# Members");
-      family.forEach(p -> 
-        pw.println(String.format(
-          "%s %s",
-          p.getGender(),
-          p.getName()
-        ))
+      family.forEach(p ->
+        pw.println(
+          String.format(
+            "%s %s",
+            p.getGender(),
+            p.getName()
+          )
+        )
       );
 
       pw.println("# Relations");
-      for (Person p : family) {
-        // If the person has children
-        if (p.iterator().hasNext()) {
-          pw.print(p.getName());
+      for (Person person : family) {
+        // Check for children
+        if (person.iterator().hasNext()) {
+          pw.print(person.getName());
 
-          for (Person child : p) {
+          for (Person child : person) {
             pw.print(" " + child.getName());
           }
           pw.println();
@@ -73,43 +65,41 @@ public class Family {
   }
 
   /**
-   * Helper method that splits a line into a list of tokens,
-   * either words or quoted names (quotes are removed).
-   * @param line – the string to tokenize
-   */
-  private static List<String> tokenize(String line) {
-    // ... no need to implement this method ...
-    return null;
+  * Helper method that splits a line into a list of tokens,
+  * either words or quoted names (quotes are removed).
+  * @param line – the string to tokenize
+  */
+  private List<String> tokenize(String line) {
+    ... no need to implement this method ...
   }
 
   /**
-   * Loads contents from the provided InputStream into this Family.
-   * @param in
-   */
+  * Loads contents from the provided InputStream into this Family.
+  * @param in
+  */
   public void load(InputStream in) throws IOException {
     try (Scanner scanner = new Scanner(in)) {
       while (scanner.hasNextLine()) {
         String line = scanner.nextLine();
 
-        // Skip first line or empty lines
         if (line.trim().length() == 0 || line.startsWith("#")) {
           continue;
         }
 
         List<String> tokens = tokenize(line);
         Gender gender = Gender.valueOf(tokens.get(0));
-        // If the line starts with a gender, - person-line
+
+        // Person-line
         if (gender != null) {
           Person newPerson = new Person(tokens.get(1));
           newPerson.setGender(gender);
           family.add(newPerson);
+        } 
         
-        // Family-tree 
-        } else {
+        // Relation-line
+        else {
           Person ancestor = findMember(tokens.get(0));
-          for (int i = 1; i < tokens.size(); i++) {
-            ancestor.addChild(findMember(tokens.get(i)));
-          }
+          tokens.stream().skip(1).forEach(p -> ancestor.addChild(new Person(p)));
         }
       }
     }
