a9c7ea44b1c76f2a3463b54591c7b04ed94889d0
Magnus L. Holtet <magnulho@gmail.com>
2019-09-02 13:43:54 +0200

    Better comments.

--- TDT4120 - Algoritmer og Datastrukturer/Practices/insertionSort.jl
+++ TDT4120 - Algoritmer og Datastrukturer/Practices/insertionSort.jl
@@ -4,14 +4,32 @@ numbers = [27, 4, 12, 13, 6, 8, 9, 13, 7, 2, 33]
 # 2. Swap other elements to the right to create the correct position and shiftthe unsorted element.
 # 3. Advance the current element to the right one element.
 
-for j = 2 : length(numbers)
-  current = numbers[j]
-  i = j - 1
-  while i > 0 && numbers[i] > current
-    numbers[i + 1] = numbers[i]
-    i = i - 1
+function sort!(list)
+  # Starting at second element
+  for j = 2 : length(list)
+    current = list[j]
+
+    # Keep index of element left of the current one
+    i = j - 1
+
+    # i > 0 => check if start of list
+    # list[i] > current => where in the list 'current' fits
+    while i > 0 && list[i] > current
+      list[i + 1] = list[i]
+      i = i - 1
+    end
+
+    # Place element
+    list[i + 1] = current
   end
-  numbers[i + 1] = current
 end
 
-println(numbers)
+function run()
+  println(numbers)
+
+  sort!(numbers)
+
+  println(numbers)
+end
+
+run()
