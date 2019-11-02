4fdbf9cfff9f0c5152322f04b628c3fd5df11f34
Magnus Lauritzen Holtet <magnulho@gmail.com>
2019-11-02 15:50:25 +0100

    Add findset.

--- TDT4120 - Algoritmer og Datastrukturer/Assignments/9/union.jl
+++ TDT4120 - Algoritmer og Datastrukturer/Assignments/9/union.jl
@@ -1,6 +1,13 @@
+function findset(x)
+    if x != x.p
+        x.p = findset(x.p)
+    end
+    x.p
+end
+
 function link(x, y)
-	if x.rank > y.rank
-		y.p = x
+   	if x.rank > y.rank
+      		y.p = x
     else
         x.p = y
         if x.rank == y.rank
