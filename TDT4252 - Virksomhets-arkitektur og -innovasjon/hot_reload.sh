7b0f1039c4bae9e94f210eee8c99986f0dcf7545
Magnus L. Holtet <dev@holtet.me>
2021-11-04 13:07:47 +0100

    Create outline for report

--- TDT4252 - Virksomhets-arkitektur og -innovasjon/hot_reload.sh
+++ TDT4252 - Virksomhets-arkitektur og -innovasjon/hot_reload.sh
@@ -9,9 +9,8 @@ trap control_c SIGINT
 kill $(lsof -ti tcp:8080)
 python3 -m http.server 8080 &
 open http://localhost:8080
-fswatch -v ./presentation.adoc | while IFS= read -r line; do
-     npx asciidoctor-revealjs presentation.adoc
-     mv presentation.html index.html
+fswatch -v ./**/*.adoc index.adoc configuration/* | while IFS= read -r line; do
+     asciidoctor -r asciidoctor-bibtex -a stem -a docinfodir="./configuration/local" index.adoc
      PID=$!
 
 done
